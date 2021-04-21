3f0d918800b69740e436ed259ec10431f4d198c8
Anthony17061995 <atoazo@hotmail.com>
2021-04-20 20:25:43 -0600

    Cambio2

--- Projecto_Final_PG4.Entidades/Entidades/Persona.cs
+++ Projecto_Final_PG4.Entidades/Entidades/Persona.cs
@@ -16,7 +16,7 @@ namespace Projecto_Final_PG4.Entidades
         
         public int ID_persona { get; set; } 
 
-         
+         //Cambio del codigo
 
         [Key]
         public string cedula { get; set; }
